286c31aa481c1160adf7b20767d27bf387fe3dfa
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2024-02-02 17:39:10 +0800

    Add files via upload

--- tasks/CRA.py
+++ tasks/CRA.py
@@ -33,20 +33,19 @@ class CRA(BaseDataset):
         min_final_2 = None
         
         for i in range(final_wanted_img_count):
-            im_path = r"C:/datasets/bdd100k_data_0.9/images/100k/val/b1d0a191-06deb55d.jpg"
-            print(f"im_path = {im_path}")
-            drivable_path,drivable_mask_path,lane_path,detection_path = self.parse_path_ver2(im_path,type=self.data_type,detect_folder=self.det_folder)
-            # print(f"{i}:{im_path_list[i]}")
-            # im = cv2.imread(im_path_list[i])
-            # h,w = im.shape[0],im.shape[1]
-            print(f"detection_path = {detection_path}")
-            print(f"{i}:{im_path}")
+            
+            drivable_path,drivable_mask_path,lane_path,detection_path = self.parse_path_ver2(im_path_list[i],type=self.data_type,detect_folder=self.det_folder)
+            print(f"{i}:{im_path_list[i]}")
+            im = cv2.imread(im_path_list[i])
+            h,w = im.shape[0],im.shape[1]
+            # print(f"detection_path = {detection_path}")
+            # print(f"{i}:{im_path}")
            
-            xywh,h,w = self.Get_CRA_XYWH(im_path,return_type=1)
+            xywh,im_h,im_w = self.Get_CRA_XYWH(im_path_list[i],return_type=1)
          
             x,y = xywh[0],xywh[1]
          
-            success = self.Add_CRA_Yolo_Txt_Label(xywh,detection_path,h,w,im_path)
+            success = self.Add_CRA_Yolo_Txt_Label(xywh,detection_path,im_h,im_w,im_path_list[i])
 
     
 
@@ -113,7 +112,6 @@ class CRA(BaseDataset):
         1: Alter Lane
         2: BackGround
         '''
-        
         drivable_path,drivable_mask_path,lane_path,detection_path = self.parse_path_ver2(im_path,type=self.data_type,detect_folder=self.det_folder)
 
         h = 0
@@ -126,38 +124,9 @@ class CRA(BaseDataset):
             print(f"{detection_path} is not exists !! PASS~~~")
             return (None,None,None,None),None,None
 
-       
-
-        # dri_map = {"MainLane": 0, "AlterLane": 1, "BackGround":2}
-        # lane_mapping: {0: 3,            1: 2,           2: 2,           3: 4,           4: 5, 
-        #       5: 2,             6: 2,           7: 4,           8: lane_bg,     9: lane_bg,
-        #       10: lane_bg,      11: lane_bg,    12: lane_bg,    13: lane_bg,    14: lane_bg,
-        #       15: lane_bg,      16: 3,          17: 1,          18: 1,          19: 4,
-        #       20: 5,            21: 1,          22: 1,          23: 4,          24: lane_bg,
-        #       25: lane_bg,      26: lane_bg,    27: lane_bg,    28: lane_bg,    29: lane_bg,
-        #       30: lane_bg,      31: lane_bg,    32: 3,          33: 2,          34: 2,  
-        #       35: 4,            36: 5,          37: 2,          38: 2,          39: 4,
-        #       40: lane_bg,      41: lane_bg,    42: lane_bg,    43: lane_bg,    44: lane_bg,
-        #       45: lane_bg,      46: lane_bg,    47: lane_bg,    48: 3,          49: 1,
-        #       50: 1,            51: 4,          52: 5,          53: 1,          54: 1,
-        #       55: 4,
-        #       255: lane_bg}
-        lane_label = {0, 16, 32, 48}
-
-        # names_lane:
-        # 0: background
-        # 1: vertical dash white line
-        # 2: vertical solid white line
-        # 3: crosswalk
-        # 4: yellow line
-        # 5: road curb
-
         xywh = (None,None,None,None)
         if os.path.exists(lane_path):
-            
             im_lane = cv2.imread(lane_path)
-            
-            
             
             tmp_left_x = 9999
             tmp_right_x = 0
@@ -167,35 +136,46 @@ class CRA(BaseDataset):
             final_left_x = 9999
             final_right_x = 9999
             final_top_y = 9999
-            final_down_y = 9999
-            ## Find the lowest X of Main lane drivable map
-            for i in range(int(h)):
-                for j in range(int(w)):
-                    if im_lane[i][j][0] != 0:
-                        print(im_lane[i][j][0])
-                        input()
-                    if im_lane[i][j][0] in lane_label:
+            final_down_y = 9999          
+            # light green  (219,211,86) --> road curb
+            # Green (127,219,86)
+            # Purpol (219,86,160)
+            # (178,96,219)
+            #Orange (86,94,219) --> cross road
+            R = 0
+            G = 0
+            B = 0
+
+            for i in range(0,int(im_h),3):
+                for j in range(0,int(im_w),3):
+                    if im_lane[i][j][0] == 86 and im_lane[i][j][1] == 94 and im_lane[i][j][2] == 219:
+                        # print(im_lane[i][j][0])
+                        # input()
                         if j < tmp_left_x:
-                            j = tmp_left_x
+                            tmp_left_x = j
                         if j > tmp_right_x:
-                            j = tmp_right_x
+                            tmp_right_x = j
                         if i < tmp_top_y:
-                            i = tmp_top_y
+                            tmp_top_y = i
                         if i > tmp_down_y:
-                            i = tmp_down_y
-                        print("find cross road label !")
+                            tmp_down_y = i 
 
             final_left_x = tmp_left_x
             final_right_x = tmp_right_x
             final_top_y = tmp_top_y
             final_down_y = tmp_down_y
-            
-            w = (final_right_x - final_left_x)
-            h = (final_down_y - final_top_y)
-            x = final_left_x + int(w/2.0)
-            y = final_top_y +  int(h/2.0)
+            print(f"{final_left_x},{final_right_x},{final_top_y},{final_down_y}")
 
-            xywh = (x,y,w,h)
+            if final_left_x!=9999:
+                w = abs(final_right_x - final_left_x)
+                h = abs(final_down_y - final_top_y)
+                x = final_left_x + int(w/2.0)
+                y = final_top_y +  int(h/2.0)
+                print(f"xywh = {x},{y},{w},{h}")
+                xywh = (x,y,w,h)
+            else:
+                xywh = (None,None,None,None)
+                w,h = None,None
            
         else:
             xywh = (None,None,None,None)
@@ -206,33 +186,13 @@ class CRA(BaseDataset):
             x2 = xywh[0] + int(xywh[2] / 2.0)
             y1 = xywh[1] - int(xywh[3] / 2.0)
             y2 = xywh[1] + int(xywh[3] / 2.0)
-            p1 = (x1,x2)
-            p2 = (y1,y2)
-            cv2.rectangle(im,p1,p2,(255,0,0),cv2.LINE_AA)
-            cv2.rectangle(im_lane,p1,p2,(255,0,0),cv2.LINE_AA)
+            p1 = (x1,y1)
+            p2 = (x2,y2)
+            cv2.rectangle(im,p1,p2,(86,94,219),2,cv2.LINE_AA)
+            cv2.rectangle(im_lane,p1,p2,(86,94,219),2,cv2.LINE_AA)
             cv2.imshow("image",im)
             cv2.imshow("im_lane",im_lane)
             cv2.waitKey()
 
         
-        return xywh,h,w
-
-
-
-                        
-
-                    
-                
-              
-                    
-
-            
-    
-
-
-
-
-
-
-
-
+        return xywh,im_h,im_w
